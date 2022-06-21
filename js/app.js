775d465c93ae8d526acd071c76a92ec799414148
Tatsuhito_Yoshikawa <yossyself_819@yahoo.co.jp>
2022-06-21 19:41:25 +0900

    feature1 tmp

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function set2fig(num) {
     return ret;
  }
 
- // 現在時刻を取得・表示する
+// 現在時刻を取得・表示する（デジタル）
 function GetTime() {
     //// 時刻取得
     // new演算子は後に来るコンストラクタのインスタンスを生成する
@@ -30,8 +30,44 @@ function GetTime() {
 
     //// 時刻表示を追記
     // htmlファイルにある要素を取得
-    var target = document.getElementById('showclock');
+    var target = document.getElementById('show_digi_clock');
     // 取得した要素に時刻表示を追記
     target.innerHTML = clock;
 };
 
+
+
+
+//// 切り替え表示の作成
+// 要素取得
+let digital_button = document.getElementById('digital_button');
+let analogue_button = document.getElementById('analogue_button');
+let show_digi_clock = document.getElementById('show_digi_clock');
+let show_ana_clock = document.getElementById('show_ana_clock');
+
+// 状態の初期化関数
+reset_styles = function() {
+    digital_button.classList.remove("active");
+    analogue_button.classList.remove("active");
+    show_digi_clock.classList.remove("active");
+    show_ana_clock.classList.remove("active");
+  };
+
+// ボタンクリックに対する挙動
+// クリックされたボタンのスタイルが活性になる
+digital_button.addEventListener("click", function() {
+    reset_styles();
+    if (this.classList.toggle("active")) {
+      show_digi_clock.classList.toggle("active");
+    }
+  })
+analogue_button.addEventListener("click", function() {
+    reset_styles();
+    if (this.classList.toggle("active")) {
+      show_ana_clock.classList.toggle("active");
+    }
+  });
+
+// デフォルトではデジタル時計のみ表示
+show_digi_clock.classList.toggle("active");
+digital_button.classList.toggle("active")
