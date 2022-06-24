158166d89c3d23014bfd9c1f19c04bd70a6f2bc5
Tatsuhito_Yoshikawa <yossyself_819@yahoo.co.jp>
2022-06-24 14:15:45 +0900

    feature1 20220624

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# exam_clock_test
+## SET→STARTの流れなら大丈夫
+## SET→STARTから再度SETの時に、ちゃんと時計が止まるようにする
+## SETの後しかSTARTが押せないように制御
+## STOPボタンも作る？
+## 見栄え 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,56 @@
 //////////// 時刻表示を秒刻みで更新する /////////////////
-setInterval(GetTimeDigital, 1000);
-setInterval(GetTimeAnalogue, 1000);
+let timerId_d = setInterval(GetTimeDigital, 1000);
+let timerId_a = setInterval(GetTimeAnalogue, 1000);
+
+//// SETボタンを押すと、指定された時刻を表示して時計が止まる
+// 時刻を指定
+const SET_TIME = "10:00:00";
+
+set_button.addEventListener("click", function() {
+  // デジタル時計のdiv要素を取得
+  var target = document.getElementById('show_digi_clock');
+  // 取得した要素に10:00:00をSET
+  target.innerHTML = SET_TIME;
+  SetTimeAnalogue(10, 0, 0);
+  // setIntervalを止める＝時計の進行を止める
+  clearInterval(timerId_d);
+  clearInterval(timerId_a);
+  }
+)
+
+//// STARTボタンを押すと、SETした時刻から時計が進み始める
+start_button.addEventListener("click", function() {
+  // 準備として、今日の日付を取得して文字列にする
+  var tmp_ymd = new Date();
+  var yyyy = tmp_ymd.getFullYear();
+  var mm = set2fig(tmp_ymd.getMonth() + 1); /* 月は1少ない数で出てくる */
+  var dd = set2fig(tmp_ymd.getDate());
+  const YMD = yyyy + "-" + mm + "-" + dd + " ";
+
+  // 指定した時刻と今日の日付を連結して、時刻型に変換
+  dt1 = new Date(YMD + SET_TIME);
+
+  // 指定した時刻から1秒ずつ増やしていく
+  let timerId_s = setInterval(function() {
+    dt1.setSeconds(dt1.getSeconds() + 1);
+    
+    // アナログ時計用に数字で取得
+    var renew_h = dt1.getHours();
+    var renew_m = dt1.getMinutes();
+    var renew_s = dt1.getSeconds();
+
+    // デジタル時計用に2桁文字列変換
+    var renew_clock = set2fig(renew_h) + ":" + set2fig(renew_m) + ":" + set2fig(renew_s);
+    
+    // デジタル時計のdiv要素を取得して表示
+    var target = document.getElementById('show_digi_clock');
+    target.innerHTML = renew_clock;
+
+    // アナログ時計の時刻設定関数を流用
+    SetTimeAnalogue(renew_h, renew_m, renew_s);
+  }, 1000)
+})
+
 
 
 /////////////////// 関数部 /////////////////////
@@ -69,6 +119,18 @@ window.onload = function () {
   }
 }
 
+// アナログ時計に特定の時刻を設定
+function SetTimeAnalogue(s_hour, s_min, s_sec) {
+  // 針の角度
+  var deg_h = s_hour * (360 / 12) + s_min * (360 / 12 / 60);
+  var deg_m = s_min * (360 / 60);
+  var deg_s = s_sec * (360 / 60);  
+
+  // それぞれの針に角度を設定
+  document.querySelector(".hour").style.transform = `rotate(${deg_h}deg)`;
+  document.querySelector(".min").style.transform = `rotate(${deg_m}deg)`;
+  document.querySelector(".sec").style.transform = `rotate(${deg_s}deg)`;
+}
 
 
 
@@ -105,3 +167,5 @@ analogue_button.addEventListener("click", function() {
 // デフォルトではデジタル時計のみ表示
 show_digi_clock.classList.toggle("active");
 digital_button.classList.toggle("active")
+
+
